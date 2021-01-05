4ca6b1b4145ec813de78452751beed7c04d55477
Przemyslaw Walkowski <przemyslaw.walkowski@snpgroup.com>
2021-01-05 09:14:43 +0100

    read works!

--- DictionaryGenerator/Program.cs
+++ DictionaryGenerator/Program.cs
@@ -10,35 +10,42 @@ namespace DictionaryGenerator
         {
             Console.WriteLine("DictionaryGenerator");
 
+            var result = new Dictionary<string, List<string>>();
+            
+            ReadDictionaryFile(args, result);
+            WritePosgreSqlScript(result);
+
+        }
+
+        private static void WritePosgreSqlScript(Dictionary<string, List<string>> result)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ReadDictionaryFile(string[] args, Dictionary<string, List<string>> result)
+        {
             var inputFilePath = new FileInfo(args[0]);
-
-
             string[] allLine = File.ReadAllLines(inputFilePath.FullName);
-
-
             var isDictionaryDefinition = true;
             var currentDictionaryDefinition = string.Empty;
-            
-            var result = new Dictionary<string, List<string>>();
 
             var valueList = new List<string>();
 
-            for (int i = 0; i < allLine.Length; i++)
+
+            for (int i = 0; i < allLine.Length + 1; i++)
             {
-                if (string.IsNullOrEmpty(allLine[i]))
+                if (allLine.Length == i || string.IsNullOrEmpty(allLine[i]))
                 {
                     isDictionaryDefinition = true;
+                    result.Add(currentDictionaryDefinition, valueList);
                     currentDictionaryDefinition = string.Empty;
+                    valueList = new List<string>();
+
                     continue;
                 }
 
                 if (isDictionaryDefinition)
                 {
-                    if (string.IsNullOrEmpty(currentDictionaryDefinition) == false)
-                    {
-                        result.Add(currentDictionaryDefinition, valueList);
-                    }
-
                     currentDictionaryDefinition = allLine[i];
                     isDictionaryDefinition = false;
                     i++;
@@ -48,9 +55,6 @@ namespace DictionaryGenerator
                 valueList.Add(allLine[i]);
 
             }
-
-            
-
         }
     }
 }
