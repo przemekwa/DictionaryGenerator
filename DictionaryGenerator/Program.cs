11b675e795e97c13989ddefef2d0f7de0fcbadf5
Przemyslaw Walkowski <przemyslaw.walkowski@snpgroup.com>
2021-01-05 09:04:21 +0100

    first attempt

--- DictionaryGenerator/Program.cs
+++ DictionaryGenerator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace DictionaryGenerator
 {
@@ -6,7 +8,49 @@ namespace DictionaryGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("DictionaryGenerator");
+
+            var inputFilePath = new FileInfo(args[0]);
+
+
+            string[] allLine = File.ReadAllLines(inputFilePath.FullName);
+
+
+            var isDictionaryDefinition = true;
+            var currentDictionaryDefinition = string.Empty;
+            
+            var result = new Dictionary<string, List<string>>();
+
+            var valueList = new List<string>();
+
+            for (int i = 0; i < allLine.Length; i++)
+            {
+                if (string.IsNullOrEmpty(allLine[i]))
+                {
+                    isDictionaryDefinition = true;
+                    currentDictionaryDefinition = string.Empty;
+                    continue;
+                }
+
+                if (isDictionaryDefinition)
+                {
+                    if (string.IsNullOrEmpty(currentDictionaryDefinition) == false)
+                    {
+                        result.Add(currentDictionaryDefinition, valueList);
+                    }
+
+                    currentDictionaryDefinition = allLine[i];
+                    isDictionaryDefinition = false;
+                    i++;
+                    continue;
+                }
+
+                valueList.Add(allLine[i]);
+
+            }
+
+            
+
         }
     }
 }
