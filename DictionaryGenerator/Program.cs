c41a1dff04ae1cdc728e2993d3a5abab10fbacae
Przemyslaw Walkowski <przemyslaw.walkowski@snpgroup.com>
2021-01-05 13:33:28 +0100

    all working

--- DictionaryGenerator/Program.cs
+++ DictionaryGenerator/Program.cs
@@ -1,25 +1,95 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
+using System.Text;
 
 namespace DictionaryGenerator
 {
     class Program
     {
+        private const string DateFormat = "yyyy-MM-dd";
+        private const string OutputFilename = "script.sql";
+
+        private static int definitionId = 0;
+        private static int valueId = 0;
+
         static void Main(string[] args)
         {
             Console.WriteLine("DictionaryGenerator");
 
             var result = new Dictionary<string, List<string>>();
-            
+            PrepareOutputFile();
             ReadDictionaryFile(args, result);
             WritePosgreSqlScript(result);
 
         }
 
+        private static void PrepareOutputFile()
+        {
+            var fileOutput = new FileInfo(OutputFilename);
+
+            if (fileOutput.Exists)
+            {
+                File.Delete(fileOutput.FullName);
+            }
+        }
+
         private static void WritePosgreSqlScript(Dictionary<string, List<string>> result)
         {
-            throw new NotImplementedException();
+            
+
+            foreach (var item in result.Keys)
+            {
+                var sb = new StringBuilder();
+
+                sb.Append("INSERT INTO public.\"DictionaryDefinitions\"(\"Id\", \"Name\", \"ParentId\", \"Created\", \"Modified\") VALUES (");
+
+                sb.AppendFormat(
+                    "{0},'{1}',{2},'{3}','{4}'", 
+                    definitionId, 
+                    item, 
+                    "null",
+                    DateTime.Now.ToString(DateFormat, CultureInfo.InvariantCulture), 
+                    DateTime.Now.ToString(DateFormat, CultureInfo.InvariantCulture));
+                sb.Append(");");
+
+                sb.AppendLine();
+                sb.AppendLine();
+
+                File.AppendAllText(OutputFilename, sb.ToString());
+
+                WriteDictionaryValues(result[item], definitionId++);
+
+            }
+        }
+
+        private static void WriteDictionaryValues(List<string> lists, int v)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var item in lists)
+            {
+                
+
+                sb.Append("INSERT INTO public.\"DictionaryValues\"(\"Id\", \"Value\", \"IsCustom\", \"DictionaryDefinitionId\", \"Created\", \"Modified\") VALUES (");
+                
+                sb.AppendFormat(
+                 "{0},'{1}','{2}',{3},'{4}','{5}'",
+                 valueId++,
+                 item,
+                 "false",
+                 v,
+                 DateTime.Now.ToString(DateFormat, CultureInfo.InvariantCulture),
+                 DateTime.Now.ToString(DateFormat, CultureInfo.InvariantCulture));
+
+                sb.Append(");");
+                sb.AppendLine();
+                sb.AppendLine();
+
+            }
+
+            File.AppendAllText(OutputFilename, sb.ToString());
         }
 
         private static void ReadDictionaryFile(string[] args, Dictionary<string, List<string>> result)
