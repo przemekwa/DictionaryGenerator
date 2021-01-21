32224f20793c8719c4d32f71022e638d0ca2b75d
Patryk Pogorzelczyk <patryk.pogorzelczyk@snpgroup.com>
2021-01-21 10:55:20 +0100

    add another custom item

--- DictionaryGenerator/Program.cs
+++ DictionaryGenerator/Program.cs
@@ -124,7 +124,8 @@ namespace DictionaryGenerator
                 || item.ToLower().Contains("jakim?")
                 || item.ToLower().Contains("rrrr")
                 || item.ToLower().Contains("(rok – miesiąc – dzień)")
-                || item.ToLower().Contains("tak/nie"))
+                || item.ToLower().Contains("tak/nie")
+                || item.ToLower().Contains("kto?"))
             {
                 return "true";
             }
